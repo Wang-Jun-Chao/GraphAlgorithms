24817d1f67365e96034869c05461dea40ab0ef8e
Wang-Jun-Chao <shining-glory@qq.com>
2016-05-15 10:05:54 +0800

    commit

--- Test/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Author: 王俊超
- * Time: 2016-05-15 09-55
- * CSDN: http://blog.csdn.net/derrantcm
- * Github: https://github.com/Wang-Jun-Chao
- * Declaration: All Rights Reserved !!!
- */
-public class Main {
-}
